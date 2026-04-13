1d0a25e0c99e0d1ac020f5d451f11132e64fd254
Moloko <molokodineo04@gmail.com>
2026-04-13 17:20:08 +0200

    The other two .cs files for the chatbot

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CybersecurityAwarenessBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CybersecurityBot bot = new CybersecurityBot();
+            bot.Start();
+        }
+    }
+}
+
+
